5fc956ba63eb49056e707bd53a4f9823c104e10b
loveaeen <1625630746@qq.com>
2023-05-31 17:15:48 +0800

    initial dom remove dynamic style

--- scripts/diverseSite.js
+++ scripts/diverseSite.js
@@ -2,8 +2,8 @@ let url = window.location.href;
 // 获取视口的宽度
 let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
-// 获取需要检查的区域宽度（左侧四分之一）
-let checkWidth = viewportWidth / 4;
+// 获取需要检查的区域宽度（左侧二十分之一）
+let checkWidth = viewportWidth / 20;
 
 if (url.includes('twitch.tv')) {
     // Twitch的处理逻辑
--- scripts/initOffset.js
+++ scripts/initOffset.js
@@ -5,21 +5,20 @@ let allElements = document.body.getElementsByTagName("*");
 let initialPositions = new Map();
 
 // 遍历所有元素，记录初始位置
-for (let i = 0; i < allElements.length; i++) {
-  let element = allElements[i];
+for (let element of allElements) {
   let rect = element.getBoundingClientRect();
   initialPositions.set(element, rect.left);
 }
 
-// 遍历所有元素
-for (let i = 0; i < allElements.length; i++) {
-  let element = allElements[i];
 
+// 遍历所有元素
+for (let element of allElements) {
   // 跳过部分元素
   if (filterAllParents(element)) continue;
 
   // 获取元素的位置信息
   let rect = element.getBoundingClientRect();
+
   // 判断元素是否在需要检查的区域内
   if (rect.left < checkWidth) {
     // 检查元素的当前位置是否与初始位置一致
@@ -35,7 +34,11 @@ function filterAllParents(element) {
   let parent = element.parentElement;
 
   while (parent) {
-    if (element.tagName === "YTD-VIDEO-PREVIEW" || parent.tagName === "YTD-VIDEO-PREVIEW") {
+    if(window.getComputedStyle(parent).getPropertyValue("display") === "none" || window.getComputedStyle(element).getPropertyValue("display") === "none"){
+      // 一些初始被隐藏的元素，如ul下的li，这些元素不再需要加入样式调整偏移量
+      return true;
+    }else if (element.tagName === "YTD-VIDEO-PREVIEW" || parent.tagName === "YTD-VIDEO-PREVIEW") {
+      // Youtube 网站偏好
       return true;
     }
     parent = parent.parentElement;
